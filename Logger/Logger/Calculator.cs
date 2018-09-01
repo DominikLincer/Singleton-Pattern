5b2b6e0d0603b1460eaa356de087e69fb7346393
Dominik Lincer <dominik.lincer@gmail.com>
2018-09-01 23:36:21 +0200

    first commt

---
+++ Logger/Logger/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Calculator
+    {
+        private readonly Logger _logger;
+
+        public Calculator()
+        {
+            _logger = Logger.GetLogger();
+        }
+
+        public void Divide(int a, int b)
+        {
+            try
+            {
+                var result = a / b;
+                Console.WriteLine(result);
+            }
+            catch (Exception exception)
+            {
+                _logger.Log(exception.Message);
+            }
+        }
+    }
+}
---
+++ Logger/Logger/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    sealed class Logger
+    {
+        private static readonly Logger _logger = new Logger();
+
+        public static Logger GetLogger()
+        {
+            return _logger;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("An exception occured: " + message);
+        }
+    }
+}
